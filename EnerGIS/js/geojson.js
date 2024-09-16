764c03637a01445e9afebdccee607a151f647373
Mert Can Yilmaz <mcyilmaz@ymail.com>
2024-09-16 19:43:53 +0200

    Test data added

--- EnerGIS/js/geojson.js
+++ EnerGIS/js/geojson.js
@@ -1,7 +1,28 @@
+// Existing elements for user-upload functionality
 var geojsonInput = document.getElementById("geojson-input");
 var geojsonBtn = document.getElementById("geojson-btn");
 var spinner = document.querySelector(".spinner");
 
+// Function to add GeoJSON layer to the map
+function addGeoJSONLayer(geojsonData) {
+  L.geoJSON(geojsonData, {
+    onEachFeature: function (feature, layer) {
+      layer.on("click", function () {
+        showBuildingPopup(feature.properties);
+      });
+    }
+  }).addTo(map);
+}
+
+// Load GeoJSON from a static file (buildings.geojson)
+fetch('data/buildings.geojson')
+  .then(response => response.json())
+  .then(geojsonData => {
+    addGeoJSONLayer(geojsonData);  // Add static GeoJSON to the map
+  })
+  .catch(error => console.error('Error loading static GeoJSON:', error));
+
+// User-uploaded GeoJSON functionality
 geojsonBtn.addEventListener("click", function () {
   geojsonInput.click();
 });
@@ -14,7 +35,7 @@ geojsonInput.addEventListener("change", function (event) {
     reader.onload = function (e) {
       try {
         var geojsonData = JSON.parse(e.target.result);
-        addGeoJSONLayer(geojsonData);
+        addGeoJSONLayer(geojsonData);  // Add user-uploaded GeoJSON to the map
       } catch (error) {
         alert("Invalid GeoJSON file.");
       } finally {
