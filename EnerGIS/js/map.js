f6ddfcdc8cfc66b27f0fb7a5130fea64bb6d1d02
Mert Can Yilmaz <mcyilmaz@ymail.com>
2024-09-25 15:07:02 +0200

    Update map.js

--- EnerGIS/js/map.js
+++ EnerGIS/js/map.js
@@ -1,18 +1,5 @@
-// Initialize the map and center it on Drohobych, Ukraine
-var map = L.map("map").setView([49.3528, 23.5055], 13);
+var map = L.map("map").setView([49.351464043421466, 23.506201004403845], 15);
 
-// Add OpenStreetMap tiles
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "&copy; OpenStreetMap contributors"
 }).addTo(map);
-
-// Function to add GeoJSON layer to the map
-function addGeoJSONLayer(geojsonData) {
-    L.geoJSON(geojsonData, {
-        onEachFeature: function (feature, layer) {
-            layer.on("click", function () {
-                showBuildingPopup(feature.properties);
-            });
-        }
-    }).addTo(map);
-}
