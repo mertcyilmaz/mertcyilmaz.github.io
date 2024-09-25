dab51b334ecae6a377893c98ab54f1acb15e7ed6
Mert Can Yilmaz <mcyilmaz@ymail.com>
2024-09-25 15:09:32 +0200

    Update map.js

--- EnerGIS/js/map.js
+++ EnerGIS/js/map.js
@@ -1,4 +1,4 @@
-var map = L.map("map").setView([49.351464043421466, 23.506201004403845], 15);
+var map = L.map("map").setView([49.351464043421466, 23.506201004403845], 20);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "&copy; OpenStreetMap contributors"
